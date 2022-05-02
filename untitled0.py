08cbaaf35d0ee67bbe775807fd74aabbc120aee0
anika-ik <79695696+anika-ik@users.noreply.github.com>
2022-05-02 15:46:30 +0530

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from PIL import ImageTk, Image
+from tkinter import filedialog
+import os
 
 root= Tk()
 root.minsize(650,650)
@@ -18,13 +20,30 @@ exit_img= ImageTk.PhotoImage(Image.open("exit.jpg"))
 text= Text(root, height= 35, width= 80 )
 text.place(relx= 0.5, rely=0.5, anchor= CENTER)
 
-open_btn= Button(root, image= open_img, text= "open file")
+name= ""
+def openfile():
+    global name
+    text.delete(1.0, END)
+    input_box.delete(0, END)
+    text_file= filedialog.askopenfilename(title= "open text file", filetypes=(("Text Files", "*.txt"),))
+    print(text_file)
+    name= os.path.basename(text_file)
+    formated_name= name.split('.')[0]
+    input_box.insert(END, formated_name)
+    root.title(formated_name)
+    text_file= open(name,'r')
+    paragraph= text_file.read()
+    text.insert(END, paragraph)
+    text_file.close()
+    
+    
+open_btn= Button(root, image= open_img, text= "open file", command= openfile)
 open_btn.place(relx= 0.05, rely= 0.03, anchor= CENTER)
 
-save_btn= Button(root, image= save_img, text= "open file")
+save_btn= Button(root, image= save_img, text= "save file")
 save_btn.place(relx= 0.10, rely= 0.03, anchor= CENTER)
 
-exit_btn= Button(root, image= exit_img, text= "open file")
+exit_btn= Button(root, image= exit_img, text= "exit file")
 exit_btn.place(relx= 0.15, rely= 0.03, anchor= CENTER)
 
 root.mainloop()
